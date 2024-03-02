14ce7cfefbc4b81350cf94bdcea5341e81501b26
AfroSenpai <credo05@hotmail.fr>
2024-03-02 23:15:59 +0000

    unzip dataset from code

--- test.py
+++ test.py
@@ -3,30 +3,47 @@ from PIL import Image
 from arrow import now
 from glob import glob
 import pandas as pd
+import matplotlib.pyplot as plt
 from os.path import basename
+
+from zipfile import ZipFile
+import patoolib
+# import pygame
+
+#unrar folder called AiArtData.rar
+
 
 img2vec = Img2Vec(cuda=True, model='resnet-18', layer='default', layer_output_size=512)
 
-GLOB_AI = './datasets/train/FAKE/*'
-GLOB_REAL = './datasets/train/REAL/*'
+FOLDER_AI = 'train/FAKE/'
+FOLDER_REAL = 'train/REAL/'
 
-def get_from_glob(arg: str, tag: str) -> list:
+def img_to_array(tag: str, zipped_folder: str = None) -> list:
     result = []
-    for count, input_file in enumerate(glob(pathname=arg)):
-        name = basename(input_file)
-        try:
-            with Image.open(fp=input_file, mode='r') as image:
-                vector = img2vec.get_vec(image, tensor=True).numpy().reshape(512,)
-                result.append(pd.Series(data=[tag, name, vector], index=['tag', 'name', 'value']))
-        except RuntimeError as error:
-            print("Runtime Error : ", error)
-            # pass
-        print("Image processed : ", count)
+    _IMG_COUNT = 100
+    count = 0
+    zip_object = ZipFile('datasets/dataset2.zip', 'r')
+    zip_object.extractall()
+    # zip_object.extract(zipped_folder, path='./datasets')
+    for input_file in zip_object.namelist():
+        if input_file.startswith(zipped_folder):
+            name = basename(input_file)
+            try:
+                with Image.open(fp=input_file, mode='r') as image:
+                    vector = img2vec.get_vec(image, tensor=True).numpy().reshape(512,)
+                    result.append(pd.Series(data=[tag, name, vector], index=['tag', 'name', 'value']))
+            except Exception as error:
+                print("Runtime Error : ", error)
+                # pass
+            if count >= _IMG_COUNT:
+                break # Stops processing images after 10 000 images processed. 
+            count += 1
+    zip_object.close()
     return result
 
 time_start = now()
-ai = get_from_glob(arg=GLOB_AI, tag='ai')
+ai = img_to_array(zipped_folder=FOLDER_AI, tag='ai')
 print('done encoding the AI images in {}'.format(now() - time_start))
-real = get_from_glob(arg=GLOB_REAL, tag='real')
+real = img_to_array(zipped_folder=FOLDER_REAL, tag='real')
 df = pd.DataFrame(data=ai + real)
 print('done in {}'.format(now() - time_start))
